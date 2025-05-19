f9c6288de542416d90588ca2d7257b1a2608637e
nec4 <42926839+nec4@users.noreply.github.com>
2025-05-19 21:12:34 +0200

    Update prepare_pytorch_data.py

--- prepare_pytorch_data.py
+++ prepare_pytorch_data.py
@@ -1,6 +1,4 @@
 import torchvision
 import subprocess
 
-me = subprocess.run("whoami", capture_output=True)
-me = me.stdout.decode('utf8').strip()
 data = torchvision.datasets.mnist.FashionMNIST(f"${WORK}/pytorch_datasets", download=True)
