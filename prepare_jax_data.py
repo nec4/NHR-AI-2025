057319b644f3ea5dcda085ddfe2f3c6944230704
nec4 <42926839+nec4@users.noreply.github.com>
2025-05-19 21:36:47 +0200

    Update prepare_jax_data.py

--- prepare_jax_data.py
+++ prepare_jax_data.py
@@ -1,6 +1,5 @@
 import tensorflow_datasets as tfds
-import subprocess
 import os
 
-os.environ["TFDS_DATA_DIR"] = f"{WORK}/jax_datasets" 
+os.environ["TFDS_DATA_DIR"] = f"{os.environ["WORK"]}/jax_datasets" 
 data = tfds.load("fashion_mnist", batch_size=512, split=["train[0%:80%]", "train[80%:100%]"])
