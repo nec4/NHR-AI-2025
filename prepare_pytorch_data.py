fec88303deac7e4ca4d4be259eb32f2d50df0d5d
nec4 <42926839+nec4@users.noreply.github.com>
2025-05-19 21:11:27 +0200

    Update prepare_pytorch_data.py

--- prepare_pytorch_data.py
+++ prepare_pytorch_data.py
@@ -3,5 +3,4 @@ import subprocess
 
 me = subprocess.run("whoami", capture_output=True)
 me = me.stdout.decode('utf8').strip()
-sub_dir="nhr_grad_school_2025"
-data = torchvision.datasets.mnist.FashionMNIST(f"/home/woody/ihpc/{me}/{sub_dir}/pytorch_datasets", download=True)
+data = torchvision.datasets.mnist.FashionMNIST(f"${WORK}/pytorch_datasets", download=True)
